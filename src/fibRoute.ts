799600cb68096c6293d87e355ed28881a7f5aaae
Jordan Lavender <jel2ub@virginia.edu>
2024-01-16 16:34:54 -0500

    import request and response and define

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,12 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const num : number = req.params;
+export default (req: Request, res: Response) => {
+  const { num }= req.params;
 
-  const fibN = fibonacci(num);
+  const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
